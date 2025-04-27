9b43fcead67f2f27a737225d3dcef62d342bc8ae
lucas2832 <lucassmsc@gmail.com>
2025-04-27 11:54:22 -0300

    Primeiro commit

---
+++ buscar-cursos.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['verify' => false, 'base_uri' => 'https://www.alura.com.br']); // não seguro para produção por conta do 'verify' => false
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach($cursos as $curso) {
+    echo $curso . "\n";
+}
